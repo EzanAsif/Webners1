a69ad9d391565a4e027576989c292abca733e576
Shayan-Irfan <ishayan767@gmail.com>
2022-06-16 20:17:19 +0500

    meta mask account reducer

---
+++ src/Store/Reducers/MetamaskAccount.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    address: {},
+    loading: false
+}
+
+const metamaskAccountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+        fillUpState: (state, action) => ({ ...state, address: { ...action.payload.user } }),
+        fillOutState: (state) => ({ ...state, address: {} }),
+        enableLoading: (state) => ({ ...state, loading: true }),
+        disableLoading: (state) => ({ ...state, loading: false }),
+    }
+});
+
+export const { fillUpState, fillOutState, enableLoading, disableLoading } = metamaskAccountSlice.actions;
+
+export default metamaskAccountSlice.reducer;
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./Reducers/AuthReducer";
 import TransactionReducer from "./Reducers/Transactions";
+import MetamaskAccountReducer from "./Reducers/MetamaskAccount";
 
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
     transactions: TransactionReducer,
+    metamaskAccount: MetamaskAccountReducer
   },
 });
